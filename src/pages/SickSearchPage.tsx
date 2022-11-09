4c0a8bb6cdce606540d91ddf69880f3b9c485bb5
pongdang <pongpong0121@kakao.com>
2022-11-09 23:40:04 +0900

    ⭐ feat: 컴포넌트 분리 및 클릭할 때 관련 검색어창 뜨도록 수정

--- src/pages/SickSearchPage.tsx
+++ src/pages/SickSearchPage.tsx
@@ -1,11 +1,22 @@
-import { Fragment, useEffect, useState } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Fragment, MouseEventHandler, useEffect, useState } from 'react';
 import { fetchSickSearchResultList } from '../apis/fetchSickSearchResultList';
+import { MainLayout } from '../components/MainLayout';
+import { SickSearchInput } from '../components/SickSearchInput';
 import { SickSearchItem } from '../components/SickSearchItem';
 import { SickItem } from '../model/SickItem';
 
 export function SickSearchPage() {
   const [searchKeyword, setSearchKeyword] = useState('');
   const [sickSearchList, setSickSearchList] = useState<SickItem[]>([]);
+  const [isOpenSearch, setOpenSearch] = useState(false);
+
+  const onSearchInputClick: MouseEventHandler<HTMLParagraphElement> = e => {
+    e.stopPropagation();
+    setOpenSearch(true);
+  };
 
   useEffect(() => {
     (async () => {
@@ -15,25 +26,59 @@ export function SickSearchPage() {
     })();
   }, [searchKeyword]);
 
-  if (sickSearchList == null || searchKeyword.trim() === '') {
+  if (sickSearchList == null || searchKeyword.trim() === '' || sickSearchList.length === 0) {
     return (
-      <>
-        <input type="search" onChange={e => setSearchKeyword(e.target.value)} />
-        <p>검색어 없음</p>
-      </>
+      <MainLayout onClick={() => setOpenSearch(false)}>
+        <SickSearchInput
+          onChange={e => setSearchKeyword(e.target.value)}
+          onClick={onSearchInputClick}
+        />
+        {isOpenSearch && (
+          <SickSearchList onClick={e => e.stopPropagation()}>
+            <p css={suggestSearchKeyword}>검색어 없음</p>
+          </SickSearchList>
+        )}
+      </MainLayout>
     );
   }
 
   return (
-    <>
-      <input type="search" onChange={e => setSearchKeyword(e.target.value)} />
-      <ul>
-        {sickSearchList.map(result => (
-          <Fragment key={result.sickCd}>
-            <SickSearchItem result={result} searchKeyword={searchKeyword} />
-          </Fragment>
-        ))}
-      </ul>
-    </>
+    <MainLayout onClick={() => setOpenSearch(false)}>
+      <SickSearchInput
+        onChange={e => setSearchKeyword(e.target.value)}
+        onClick={onSearchInputClick}
+      />
+
+      {isOpenSearch && (
+        <SickSearchList>
+          <li css={suggestSearchKeyword}>추천 검색어</li>
+          {sickSearchList
+            .filter(result => result.sickNm.match(new RegExp(searchKeyword, 'i')))
+            .map(filterResult => (
+              <Fragment key={filterResult.sickCd}>
+                <SickSearchItem result={filterResult} searchKeyword={searchKeyword} />
+              </Fragment>
+            ))}
+        </SickSearchList>
+      )}
+    </MainLayout>
   );
 }
+
+const SickSearchList = styled.ul`
+  width: 100%;
+  max-width: 490px;
+  width: 100%;
+  margin-top: 10px;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 20px 10px;
+  box-sizing: border-box;
+`;
+
+const suggestSearchKeyword = css`
+  padding: 10px 20px;
+  font-size: 1.3rem;
+  color: #a7afb7;
+`;
