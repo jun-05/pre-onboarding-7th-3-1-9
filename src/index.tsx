36700828520352e5ffdca46b0e0d9611235eec3c
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-11-10 23:39:26 +0900

    ⭐ feat : Theme 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from './styles/Theme';
 import store from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
+  <ThemeProvider theme={Theme}>
   <Provider store={store}>
     <App />
   </Provider>
+  </ThemeProvider>
 );
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,18 @@
+export const Theme = {
+  // brand color
+  // background
+  backgroundColor: '#CAE9FF',
+
+  mainColor: '#357AE1',
+
+  // achromatic color
+  white: '#ffffff',
+  lightgray: '#F0F0F0',
+  gray: '#C9CED3',
+  darkgray: '#6B6B6B',
+  black: '#000000',
+
+  // transition
+  transition: 'all 0.3s',
+};
+
