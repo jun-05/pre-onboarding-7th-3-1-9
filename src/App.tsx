a93baf0a97fb87f5e8185a3746476a3068774604
LeeChangHoon <changhoon1030@gmail.com>
2022-11-10 21:00:41 +0900

    ⭐ feat : App.js 컴포넌트 분리 및 css 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,47 @@
-import React, { useState, useEffect,useRef } from 'react';
-import styled from 'styled-components';
-import SearchIcon from './assets/SearchIcon'
+import React, { KeyboardEvent, useState, useEffect } from 'react';
 import Title from './components/Title'
 import SearchBar from './components/SearchBar';
 import { getSick } from './api/cache';
-
-interface autoDatas {
-  sickCd: string;
-  sickNm: string;
-}
-
-function App() {
-  const autoRef = useRef(null)
+import { autoDatas } from './interface/types';
+import AutoSearchDataContainer from './container/AutoSearchDataContainer/AutoSearchDataContainer';
+import { Layout, SearchContainer } from './components/style';
+const App = () => {
   const [keyword, setKeyword] = useState<string>("");
   const [keyItems, setKeyItems] = useState<autoDatas[]>([]);
+  const [selectIndex, setSelectIndex] = useState<number>(-1);
+
+  const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (keyword == null) {
+      return;
+    }
+
+    const maxIndex = keyItems.length - 1;
+    if (e.key === 'ArrowUp') {
+      if (selectIndex === -1) {
+        setSelectIndex(maxIndex);
+      } else {
+        setSelectIndex(selectIndex - 1);
+      }
+    }
+
+    if (e.key === 'ArrowDown') {
+      if (selectIndex === maxIndex) {
+        setSelectIndex(-1);
+      } else {
+        setSelectIndex(selectIndex + 1);
+      }
+    }
+  };
+
   const onChangeData = (e: React.FormEvent<HTMLInputElement>) => {
     setKeyword(e.currentTarget.value);
   };
 
-
   useEffect(() => {
-
     const debounce = setTimeout(() => {
-      if (keyword){
+      if (keyword) {
         const data = getSick(keyword)
-        data.then((res)=>setKeyItems(res))
+        data.then((res) => setKeyItems(res))
       }
     }, 200)
     return () => {
@@ -35,84 +52,15 @@ function App() {
   return (
     <Layout>
       <SearchContainer>
-        <Title topContent={'국내 모든 임상시험 검색하고'}  bottomContent={'온라인으로 참여하기'}/>
-        <SearchBar keyword={keyword} onChangeData={onChangeData}/>
+        <Title topContent={'국내 모든 임상시험 검색하고'} bottomContent={'온라인으로 참여하기'} />
+        <SearchBar keyword={keyword} onChangeData={onChangeData} onKeyUp={onKeyUp} />
         {keyItems.length > 0 && keyword && (
-          <AutoSearchContainer>
-            <AutoSearchWrap ref={autoRef}>
-              <>
-              <Recommended>추천 검색어</Recommended>
-              {keyItems.map((search, idx) => (
-                <AutoSearchData
-                  key={search.sickCd}
-                  onClick={() => {
-                    setKeyword(search.sickNm);
-                  }}
-                >
-                    <SearchIcon />
-                  <div>{search.sickNm}</div>
-                </AutoSearchData>
-              ))}
-              </>
-            </AutoSearchWrap>
-          </AutoSearchContainer>
+            <AutoSearchDataContainer keyItems={keyItems} selectIndex={selectIndex} setKeyword={setKeyword} />
         )}
       </SearchContainer>
     </Layout>
   );
 }
 
-const Layout = styled.div`
-  display:flex;
-  justify-content:center;
-  text-align:center;
-  background-color: #D0E8FD;
-  height:100vh;
-`
-
-
-const SearchContainer = styled.div`
-  width: 400px;
-  height: 45px;
-  position: relative;
-  border: 0;
-`;
-
-
-const AutoSearchContainer = styled.div`
-  z-index: 3;
-  height: 50vh;
-  width: 400px;
-  background-color: #fff;
-  position: absolute;
-  top: 180px;
-  border-radius: 1rem;
-  padding: 15px 0rem;
-`;
-
-const AutoSearchWrap = styled.ul`
-  list-style:none;
-`;
-
-const AutoSearchData = styled.li`
-  display:flex;
-  padding: 10px;
-  font-size: 14px;
-  font-weight: bold;
-  z-index: 4;
-  letter-spacing: 2px;
-  /* border:1px solid red; */
-  &:hover {
-    background-color: #edf5f5;
-    cursor: pointer;
-  }
-  position: relative;
-`;
-
-const Recommended =styled.div`
-  font-size: 0.8rem;
-  color: #B1B9C0;
-  text-align:left;
-`
 
 export default App;
---
+++ src/components/style.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display:flex;
+  justify-content:center;
+  text-align:center;
+  background-color: #D0E8FD;
+  height:100vh;
+`
+
+
+export const SearchContainer = styled.div`
+  width: 400px;
+  height: 45px;
+  position: relative;
+  border: 0;
+`;
