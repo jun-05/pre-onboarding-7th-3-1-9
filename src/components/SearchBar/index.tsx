991a0148afe5a21cb0b6a982111bab9f4c049ffa
LeeChangHoon <changhoon1030@gmail.com>
2022-11-10 21:00:19 +0900

    ⭐ feat : 방향키로 검색어 선택 기능

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -3,17 +3,18 @@ import SearchIcon from '../../assets/SearchIcon'
 
 interface ISearch{
   keyword : string;
-  onChangeData: any
+  onChangeData: any;
+  onKeyUp : any;
 }
 
 
-const SearchBar = ({keyword, onChangeData}:ISearch) => {
+const SearchBar = ({keyword, onChangeData, onKeyUp}:ISearch) => {
   return(
     <InputWrapper>
     <IconWrapper>
       <SearchIcon />
     </IconWrapper>
-    <Search value={keyword} onChange={onChangeData} />
+    <Search value={keyword} onChange={onChangeData} onKeyUp={onKeyUp} />
     <BlueBox>검색</BlueBox>
   </InputWrapper>
   )
---
+++ src/components/SearchBar/style.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const InputWrapper = styled.section`
+  display:flex;
+  background-color:#ffffff;
+  border-radius: 1rem;
+  align-items:center;
+`
+
+export const Search = styled.input`
+  border: 0;
+  padding-left: 10px;
+  width: 100%;
+  height: 100%;
+  outline: none;
+`;
+
+export const BlueBox = styled.div`
+  color: #ffffff;
+  background-color:#367AE1;
+  padding:0.7rem;
+  border-radius: 0 1rem 1rem 0rem;
+  min-width:3rem;
+  border:1px solid #367AE1;
+`
+
+export const IconWrapper = styled.section`
+    padding:0.7rem;
+    border-radius: 1rem 0 0 1rem;
+`
