a6343aa352a62f4892e3dbec30abdf874494cc2d
LeeChangHoon <changhoon1030@gmail.com>
2022-11-11 01:47:32 +0900

    ⭐ feat : 추천 검색어 중 검색키워드에 해당하는 부분 Bold처리

--- src/components/AutoSearchData/index.tsx
+++ src/components/AutoSearchData/index.tsx
@@ -1,13 +1,18 @@
-import {AutoSearchDataLi} from './style'
+import { AutoSearchDataLi } from './style'
 import SearchIcon from '../../assets/SearchIcon'
-import {IAutoSearchData} from '../../interface/types'
+import { IAutoSearchData } from '../../interface/types'
 
-
-const AutoSearchData = ({key, selectIndex, index, name, setKeyword} : IAutoSearchData) => {
-  return(
-    <AutoSearchDataLi className={index === selectIndex ? 'select' : ''} key={key} onClick={()=>setKeyword(name)}>
+const AutoSearchData = ({ key, selectIndex, index, name, setKeyword, keyword }: IAutoSearchData) => {
+  const textArray = name.split(keyword);
+  return (
+    <AutoSearchDataLi className={index === selectIndex ? 'select' : ''} key={key} onClick={() => setKeyword(name)} >
       <SearchIcon />
-      <div>{name}</div>
+      {textArray.map((item, i) => (
+        <span key={i} className="search_text">
+          {item}
+          {i !== textArray.length - 1 && <span className="font_bold">{keyword}</span>}
+        </span>
+      ))}
     </AutoSearchDataLi>
   )
 }
