9c5ef6b33f301296026dde01877df21b4efd68d5
pongdang <pongpong0121@kakao.com>
2022-11-10 14:56:43 +0900

    📝 style: 주석 제거 및 코드간격 조절

--- src/utils/cacheStorage.ts
+++ src/utils/cacheStorage.ts
@@ -14,7 +14,7 @@ function createCacheStorage() {
 
         const expiresDate = parseExpires(cached.expireDate);
         if (isExpire(expiresDate)) {
-          console.log('remove', decodeURI(key));
+          console.log('지운다', decodeURI(key));
           cacheStorage.remove(`${prefixKey}-${decodeURI(key)}`);
 
           return null;
@@ -25,11 +25,13 @@ function createCacheStorage() {
         return null;
       }
     },
+
     set: async (key: string, value: SickItem, expireSeconds: number) => {
       const expireDate = getExpires(expireSeconds);
 
       localStorage.setItem(`${prefixKey}-${decodeURI(key)}`, JSON.stringify({ expireDate, value }));
     },
+
     remove: (key: string) => {
       localStorage.removeItem(`${prefixKey}-${decodeURI(key)}`);
     },
@@ -38,29 +40,20 @@ function createCacheStorage() {
   return cacheStorage;
 }
 
-// > isExpire(new Date(Date.now()))
-// false
-// > isExpire(new Date(Date.now() - 1))
-// true
+function parseExpires(expires: string) {
+  return new Date(Date.parse(expires));
+}
+
 function isExpire(date: Date) {
   // NOTE: 유효하지 않은 날짜는 만료로 처리합니다.
   if (isNaN(date.getTime())) {
     return true;
   }
 
+  // 현재 날짜가 더 크면 만료 -> true
   return Date.now() > date.getTime();
 }
 
-// > parseExpires('Fri, 07 Oct 2022 06:07:56 GMT');
-// => UTC 문자열을 파싱하여 구한 Date 객체
-// 2022-10-07T06:07:56.000Z
-function parseExpires(expires: string) {
-  return new Date(Date.parse(expires));
-}
-
-// > getExpires(100)
-// => 100초 뒤를 의미하는 Date 의 UTC 문자열
-// => Fri, 07 Oct 2022 06:07:56 GMT
 function getExpires(seconds: number) {
   return new Date(Date.now() + seconds * 1000).toUTCString();
 }
